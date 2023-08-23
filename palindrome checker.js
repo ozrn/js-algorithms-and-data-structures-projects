3a10a3da85c1525f538df97bf1ab60228f18c33c
kevser özeren <ozerenkevser92@gmail.com>
2023-08-23 11:45:49 -0400

    Use build-in functions to check if str is palindrome

--- palindrome checker.js
+++ palindrome checker.js
@@ -7,3 +7,11 @@
 // We'll pass strings with varying formats, such as racecar, RaceCar, and race CAR among others.
 
 // We'll also pass strings with special symbols, such as 2A3*3a2, 2A3 3a2, and 2_A3*3#A2.
+
+function palindrome(str) {
+    const reg = /[^A-Za-z0-9]/g
+    const lowRegStr = str.toLowerCase().replace(reg, '')
+    const reverseStr = lowRegStr.split('').reverse().join('')
+    return reverseStr === lowRegStr
+}
+palindrome("A man, a plan, a canal. Panama")
